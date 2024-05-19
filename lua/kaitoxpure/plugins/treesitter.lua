94ccd1d4cdfb2ca03b265aade462beb01e183238
Gerhard Rahn <gerhard.rahn1@gmail.com>
2024-05-19 21:49:11 +0100

    amend treesitter to lazy

--- lua/kaitoxpure/plugins/treesitter.lua
+++ lua/kaitoxpure/plugins/treesitter.lua
@@ -1,39 +1,66 @@
-local status, treesitter = pcall(require, "nvim-treesitter.configs")
-if not status then
-	return
-end
+-- code highlighting see docs for examples
+return {
+  "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPre", "BufNewFile" },
+  build = ":TSUpdate",
+  dependencies = {
+    "windwp/nvim-ts-autotag",
+  },
+  config = function()
+    -- import nvim-treesitter plugin
+    local treesitter = require("nvim-treesitter.configs")
 
-treesitter.setup({
-	highlight = { enable = true },
-	indent = { enable = true },
-	autotag = { enable = true },
-	ensurue_installed = {
-		"json",
-		"javascript",
-		"typescript",
-		"tsx",
-		"yaml",
-		"html",
-		"css",
-		"markdown",
-		"lua",
-		"vim",
-		"dockerfile",
-		"gitignore",
-		"go",
-		"gomod",
-		"gosum",
-		"gotmpl",
-		"templ",
-		"gowork",
-		"java",
-		"python",
-		"regex",
-		"rust",
-		"sql",
-		"c",
-		"c_sharp",
-		"c_sharp",
-	},
-	auto_install = true,
-})
+    -- configure treesitter
+    treesitter.setup({ -- enable syntax highlighting
+      highlight = {
+        enable = true,
+      },
+      -- enable indentation
+      indent = { enable = true },
+      -- enable autotagging (w/ nvim-ts-autotag plugin)
+      autotag = {
+        enable = true,
+      },
+      -- ensure these language parsers are installed
+      ensure_installed = {
+        "json",
+        "javascript",
+        "typescript",
+        "tsx",
+        "yaml",
+        "html",
+        "css",
+        "markdown",
+        "markdown_inline",
+        "lua",
+        "vim",
+        "dockerfile",
+        "gitignore",
+        "go",
+        "gomod",
+        "gosum",
+        "gowork",
+        "gotmpl",
+        "templ",
+        "java",
+        "python",
+        "regex",
+        "rust",
+        "sql",
+        "c",
+        "c_sharp",
+        "c_sharp",
+        "vimdoc",
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+    })
+  end,
+}
