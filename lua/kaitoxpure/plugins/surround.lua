4586a76af2ed1d42b619cd7850aff41e729a1eee
Gerhard Rahn <gerhard.rahn1@gmail.com>
2024-05-19 21:53:24 +0100

    surround plugin

---
+++ lua/kaitoxpure/plugins/surround.lua
@@ -0,0 +1,7 @@
+-- surround with x: e.g. ys{w}{""} to surround word with double quotes
+return {
+  "kylechui/nvim-surround",
+  event = { "BufReadPre", "BufNewFile" },
+  version = "*", -- Use for stability; omit to use `main` branch for the latest features
+  config = true,
+}
