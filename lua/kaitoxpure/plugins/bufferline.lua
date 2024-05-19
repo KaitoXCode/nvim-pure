670c0528c4a301142afc0bd4fa50e0ce2ec242a0
Gerhard Rahn <gerhard.rahn1@gmail.com>
2024-05-19 21:50:48 +0100

    better tabs for buffers

---
+++ lua/kaitoxpure/plugins/bufferline.lua
@@ -0,0 +1,11 @@
+-- better tabs for buffers + better icons with dep
+return {
+  "akinsho/bufferline.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  version = "*",
+  opts = {
+    options = {
+      mode = "tabs",
+    },
+  },
+}
