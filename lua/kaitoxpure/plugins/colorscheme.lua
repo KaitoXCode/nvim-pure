9228be49083e623709c50585ddc5d1f2738f5fda
Gerhard Rahn <gerhard.rahn1@gmail.com>
2024-05-21 21:57:12 +0100

    adjust tokio theme + make comments green

--- lua/kaitoxpure/plugins/colorscheme.lua
+++ lua/kaitoxpure/plugins/colorscheme.lua
@@ -32,3 +32,63 @@
 --   end,
 -- }
 
+-- custom implementation of tokyonight theme
+return {
+  "folke/tokyonight.nvim",
+  priority = 1000,
+  config = function()
+    local transparent = false -- set to true if you would like to enable transparency
+
+    local bg = "#011628"
+    local bg_dark = "#011423"
+    local bg_highlight = "#143652"
+    local bg_search = "#0A64AC"
+    local bg_visual = "#275378"
+    local fg = "#CBE0F0"
+    local fg_dark = "#B4D0E9"
+    local fg_gutter = "#627E97"
+    local border = "#547998"
+    local cmt_green = "#9ECE6A"
+
+    require("tokyonight").setup({
+      style = "night",
+      transparent = transparent,
+      terminal_colors = true,
+      styles = {
+        comments = { italic = true },
+        keywords = { italic = true },
+        functions = { bold = true },
+        sidebars = transparent and "transparent" or "dark",
+        floats = transparent and "transparent" or "dark",
+      },
+      --@param colors ColorScheme
+      on_colors = function(colors)
+        colors.comment = cmt_green
+        colors.bg = bg
+        colors.bg_dark = transparent and colors.none or bg_dark
+        colors.bg_float = transparent and colors.none or bg_dark
+        colors.bg_highlight = bg_highlight
+        colors.bg_popup = bg_dark
+        colors.bg_search = bg_search
+        colors.bg_sidebar = transparent and colors.none or bg_dark
+        colors.bg_statusline = transparent and colors.none or bg_dark
+        colors.bg_visual = bg_visual
+        colors.border = border
+        colors.fg = fg
+        colors.fg_dark = fg_dark
+        colors.fg_float = fg
+        colors.fg_gutter = fg_gutter
+        colors.fg_sidebar = fg_dark
+      end,
+      --@Param highlights Highlights
+      --@param colors ColorScheme
+      on_highlights = function(highlights, colors)
+        colors.comment = cmt_green
+      end,
+    })
+
+    vim.cmd("colorscheme tokyonight")
+  end,
+}
+
+-- return {}
