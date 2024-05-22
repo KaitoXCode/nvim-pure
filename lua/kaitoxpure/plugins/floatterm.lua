e373be7cba2e9d695941869bd09cca65a1cfd2d1
UwUMaster69 <gerhard.rahn@fasanara.com>
2024-05-22 11:01:50 +0000

    more floating term mappings

--- lua/kaitoxpure/plugins/floatterm.lua
+++ lua/kaitoxpure/plugins/floatterm.lua
@@ -10,6 +10,9 @@ return {
     -- set keymaps
     local keymap = vim.keymap.set -- for conciseness
     keymap({ "n", "t" }, "<leader>tt", "<cmd>FloatermToggle<CR>", { desc = "Toggle floating terminal" })
+    keymap({ "n", "t" }, "<leader>tc", "<cmd>FloatermNew<CR>", { desc = "Create floating terminal" })
+    keymap({ "n", "t" }, "<leader>tn", "<cmd>FloatermNext<CR>", { desc = "Next floating terminal" })
+    keymap({ "n", "t" }, "<leader>tp", "<cmd>FloatermPrev<CR>", { desc = "Prev floating terminal" })
     -- floaterm.setup()
   end,
 }
