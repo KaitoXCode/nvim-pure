3c8d2f6cbcef274e57555ed116e4c02206bfb56c
UwUMaster69 <gerhard.rahn@fasanara.com>
2024-05-22 11:01:04 +0000

    move tab keymaps to T

--- lua/kaitoxpure/core/keymaps.lua
+++ lua/kaitoxpure/core/keymaps.lua
@@ -13,10 +13,10 @@ keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width
 keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
 
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>") -- open new tab
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>") -- close current tab
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>") -- go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>") -- go to prev tab
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>") -- mv buffer to next tab
+keymap.set("n", "<leader>To", "<cmd>tabnew<CR>") -- open new tab
+keymap.set("n", "<leader>Tx", "<cmd>tabclose<CR>") -- close current tab
+keymap.set("n", "<leader>Tn", "<cmd>tabn<CR>") -- go to next tab
+keymap.set("n", "<leader>Tp", "<cmd>tabp<CR>") -- go to prev tab
+keymap.set("n", "<leader>Tf", "<cmd>tabnew %<CR>") -- mv buffer to next tab
 
 keymap.set("n", ";", ":") -- easier cmd mode
