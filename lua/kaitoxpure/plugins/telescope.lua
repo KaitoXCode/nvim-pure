dd52c6e5609b73f1ab1d95dfab3b8ca7e504e21f
UwUMaster69 <gerhard.rahn@fasanara.com>
2024-05-29 08:41:05 +0000

    more convenient mapping for telescope find old files

--- lua/kaitoxpure/plugins/telescope.lua
+++ lua/kaitoxpure/plugins/telescope.lua
@@ -43,7 +43,7 @@ return {
     local keymap = vim.keymap -- for conciseness
 
     keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Fuzzy find files in cwd" })
-    keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<CR>", { desc = "Fuzzy find recent files" })
+    keymap.set("n", "<leader>fl", "<cmd>Telescope oldfiles<CR>", { desc = "Fuzzy find recent files" })
     keymap.set("n", "<leader>fw", "<cmd>Telescope live_grep<CR>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<CR>", { desc = "Find string under cursor in cwd" })
     keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>", { desc = "Find buffers" })
