6094a07fb7fb68822f0977578c59f79b0d2936ac
UwUMaster69 <gerhard.rahn@fasanara.com>
2024-07-08 13:53:42 +0000

    add folds configuration

--- lua/kaitoxpure/lazy.lua
+++ lua/kaitoxpure/lazy.lua
@@ -11,6 +11,29 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- FOLDS CONFIG
+-- Create an augroup to manage the autocommands
+local augroup = vim.api.nvim_create_augroup("MyAutocommands", {})
+-- Autocommand to set foldmethod to 'indent' before reading the buffer
+vim.api.nvim_create_autocmd("BufReadPre", {
+  group = augroup,
+  pattern = "*",
+  callback = function()
+    vim.opt_local.foldmethod = "indent"
+  end,
+})
+-- Autocommand to set foldmethod to 'manual' when entering the buffer window
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  group = augroup,
+  pattern = "*",
+  callback = function()
+    if vim.opt_local.foldmethod:get() == "indent" then
+      vim.opt_local.foldmethod = "manual"
+    end
+  end,
+})
+-- FOLDS CONFIG
+
 require("lazy").setup({
   { import = "kaitoxpure.plugins" },
   { import = "kaitoxpure.plugins.lsp" },
